06c98e762072dd3cb2331c6b564485fa495767a3
eyazici <eyazici@sfu.ca>
2021-02-13 18:20:40 -0800

    vertical bars added

--- visualize.py
+++ visualize.py
@@ -7,13 +7,27 @@ import time
 WIDTH = 800
 HEIGHT = 600
 
+num_bars = 10
+bar_width = 20
+space = 5
+hor_offset = WIDTH/4
+
+
+def drawBar(screen, x, height):
+    pygame.draw.rect(screen, (0,0,0), (x, 400-height, bar_width, height), 0)
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
     screen.fill((255, 255, 255))
-    pygame.display.update()
+
+    for i in range(num_bars):
+        height = random.randint(10, 100)
+        x = hor_offset + (i*bar_width) + (i*space)
+        drawBar(screen, x, height)
+
     while (True):
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
