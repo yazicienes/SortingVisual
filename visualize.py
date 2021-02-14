98283d74520ba8f701bbac0a7f81dd19ec721431
eyazici <eyazici@sfu.ca>
2021-02-13 18:06:52 -0800

    game main loop

--- visualize.py
+++ visualize.py
@@ -4,4 +4,19 @@ import random
 import time
 
 #constans
+WIDTH = 800
+HEIGHT = 600
 
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    screen.fill((255, 255, 255))
+    pygame.display.update()
+    while (True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+main()
